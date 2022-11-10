4f3cc5dc8e98ef6ce621684684d3872c7ae62e66
Martin Andráši <andrasi.m007@gmail.com>
2022-11-10 23:02:51 +0100

    init auth

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from '@app/user/user.module'
+export * from '@app/user/user.service'
+export * from '@app/user/user.resolver'
+export * from '@app/user/entities/user.entity'
+export * from '@app/auth/auth.module'
+export * from '@app/auth/auth.service'
--- src/user/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user.module'
-export * from './user.service'
-export * from './user.resolver'
-export * from './entities/user.entity'
