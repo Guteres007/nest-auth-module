5e6d0dad051b59d1672fb1cb5e685772d20e2bf5
Martin Andráši <andrasi.m007@gmail.com>
2022-11-10 19:53:09 +0100

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _ = require("lodash");
+function chunk() {
+    console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as _ from 'lodash'
+
+
+function chunk() {
+    console.log(
+        _.chunk(['a', 'b', 'c', 'd'], 2)
+    )
+}
